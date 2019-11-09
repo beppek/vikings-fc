e7040ccca66cf2e3af266041fb305631b05955fd
Max Karlsson <max@dotdev.com.au>
2019-11-09 12:31:05 +1100

    🐛 Remove unused regiester link resolver

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,12 +3,6 @@
  *
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
-
-const { registerLinkResolver } = require("gatsby-source-prismic-graphql")
-const { linkResolver } = require("./src/utils/linkResolver")
-
-registerLinkResolver(linkResolver)
-
 export const onClientEntry = () => {
   // IntersectionObserver polyfill for gatsby-background-image (Safari, IE)
   if (typeof window.IntersectionObserver === `undefined`) {
